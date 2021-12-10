fe38e7b1d1f1261cec567cf70b763a686ba9c818
Andriyanov322 <BombatGame@gmail.com>
2021-12-10 15:49:05 +0500

    Добавил кнопки для будущих упражнений и задач

--- index.php
+++ index.php
@@ -17,10 +17,21 @@
 	<li><a href="lab-1-1.php" class="current">Упражнение 1-2</a></li>
 	<li><a href="lab-1-2.php" class="current">Упражнение 1-3</a></li>
 	<li><a href="lab-1-3.php" class="current">Упражнение 1-4</a></li>
+	<li><a href="lab-2-1.php" class="current">Упражнение 1-6</a></li>
+	<li><a href="lab-2-2.php" class="current">Упражнение 1-7</a></li>
+	<li><a href="lab-2-3.php" class="current">Упражнение 1-8</a></li>
+	<li><a href="lab-2-4.php" class="current">Упражнение 1-9</a></li>
+	<li><a href="lab-2-5.php" class="current">Упражнение 1-10</a></li>
+	<li><a href="lab-2-6.php" class="current">Упражнение 1-11</a></li>
+	<li><a href="lab-2-7.php" class="current">Упражнение 1-12</a></li>
+	<li><a href="lab-2-8.php" class="current">Упражнение 1-13</a></li>
+	<li><a href="lab-2-9.php" class="current">Упражнение 1-14</a></li>
 </ul>
 <p> Самостоятельная работа</p>
 <ul class="work_menu">
 	<li><a href="lab-1-5.php" class="current">Задача 1-1</a></li>
+	<li><a href="lab-2-10.php" class="current">Задача 1-2</a></li>
+	<li><a href="lab-2-11.php" class="current">Задача 1-3</a></li>
 </ul>
 	</body>
 </html>
