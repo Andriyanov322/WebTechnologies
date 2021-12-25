09b2f54ed53a01a6cf0f3db4f2d21f767c8a89a6
Andriyanov322 <79974072+Andriyanov322@users.noreply.github.com>
2021-12-26 03:29:10 +0500

    Добавил задания 3 лабораторной

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html>
 <title> Главная страница Андриянова Максима</title>
+<table border="0" cellspacing="20">
 <table border="0" cellspacing="30">
-	<head>
-		<meta charset="utf-8">
-	</head>
-	<body>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
    <tr>
     <th align="left">Лабораторная работа №1:</th>
     <th align="left">Лабораторная работа №2:</th>
-	<th align="left">Лабораторная работа №3:</th>
+    <th align="left">Лабораторная работа №3:</th>
    </tr>
 <tr>
 <td>
@@ -21,25 +22,25 @@
 ?>
 <p> Упражнения</p>
 <ul class="ex_menu">
-	<li><a href="index.php" class="current">Упражнение 1-1(Внутрення ссылка)</a></li>
-	<li><a href="lab-1/lab-1-1.php" class="current">Упражнение 1-2</a></li>
-	<li><a href="lab-1/lab-1-2.php" class="current">Упражнение 1-3</a></li>
-	<li><a href="lab-1/lab-1-3.php" class="current">Упражнение 1-4</a></li>
-	<li><a href="lab-1/lab-2-1.php" class="current">Упражнение 1-6</a></li>
-	<li><a href="lab-1/lab-2-2.php" class="current">Упражнение 1-7</a></li>
-	<li><a href="lab-1/lab-2-3.php" class="current">Упражнение 1-8</a></li>
-	<li><a href="lab-1/lab-2-4.php" class="current">Упражнение 1-9</a></li>
-	<li><a href="lab-1/lab-2-5.php" class="current">Упражнение 1-10</a></li>
-	<li><a href="lab-1/lab-2-6.php" class="current">Упражнение 1-11</a></li>
-	<li><a href="lab-1/lab-2-7.php" class="current">Упражнение 1-12</a></li>
-	<li><a href="lab-1/lab-2-8.php" class="current">Упражнение 1-13</a></li>
-	<li><a href="lab-1/lab-2-9.php" class="current">Упражнение 1-14</a></li>
+    <li><a href="index.php" class="current">Упражнение 1-1(Внутрення ссылка)</a></li>
+    <li><a href="lab-1/lab-1-1.php" class="current">Упражнение 1-2</a></li>
+    <li><a href="lab-1/lab-1-2.php" class="current">Упражнение 1-3</a></li>
+    <li><a href="lab-1/lab-1-3.php" class="current">Упражнение 1-4</a></li>
+    <li><a href="lab-1/lab-2-1.php" class="current">Упражнение 1-6</a></li>
+    <li><a href="lab-1/lab-2-2.php" class="current">Упражнение 1-7</a></li>
+    <li><a href="lab-1/lab-2-3.php" class="current">Упражнение 1-8</a></li>
+    <li><a href="lab-1/lab-2-4.php" class="current">Упражнение 1-9</a></li>
+    <li><a href="lab-1/lab-2-5.php" class="current">Упражнение 1-10</a></li>
+    <li><a href="lab-1/lab-2-6.php" class="current">Упражнение 1-11</a></li>
+    <li><a href="lab-1/lab-2-7.php" class="current">Упражнение 1-12</a></li>
+    <li><a href="lab-1/lab-2-8.php" class="current">Упражнение 1-13</a></li>
+    <li><a href="lab-1/lab-2-9.php" class="current">Упражнение 1-14</a></li>
 </ul>
 <p> Самостоятельная работа</p>
 <ul class="work_menu">
-	<li><a href="lab-1/lab-1-5.php" class="current">Задача 1-1</a></li>
-	<li><a href="lab-1/lab-2-10.php" class="current">Задача 1-2</a></li>
-	<li><a href="lab-1/lab-2-11.php" class="current">Задача 1-3</a></li>
+    <li><a href="lab-1/lab-1-5.php" class="current">Задача 1-1</a></li>
+    <li><a href="lab-1/lab-2-10.php" class="current">Задача 1-2</a></li>
+    <li><a href="lab-1/lab-2-11.php" class="current">Задача 1-3</a></li>
 </ul>
 </td>
 <td style="vertical-align:top">
@@ -57,23 +58,25 @@
 </td>
 <td style="vertical-align:top">
 <ul>
-  <li><a href="">Упражнение 3-1</a></li>
-  <li><a href="">Упражнение 3-2</a></li>
-  <li><a href="">Упражнение 3-3</a></li>
-  <li><a href="">Упражнение 3-4</a></li>
-  <li><a href="">Упражнение 3-5</a></li>
-  <li><a href="">Упражнение 3-6</a></li>
-  <li><a href="">Упражнение 3-7</a></li>
+  <li><a href="laba-3/f1.html">Упражнение 3-1</a></li>
+  <li><a href="laba-3/f2.html">Упражнение 3-2</a></li>
+  <li><a href="laba-3/f3.html">Упражнение 3-3</a></li>
+  <li><a href="laba-3/f4.html">Упражнение 3-4</a></li>
+  <li><a href="laba-3/f5.html">Упражнение 3-5</a></li>
+  <li><a href="laba-3/f6.html">Упражнение 3-6</a></li>
+  <li><a href="laba-3/f7.html">Упражнение 3-7</a></li>
 </ul>
 <p> Самостоятельная работа </p>
 <ul>
-  <li><a href="">Задача 3-1</a></li>
-  <li><a href="">Задача 3-2</a></li>
-  <li><a href="">Задача 3-3</a></li>
-  <li><a href="">Задание 3-4</a></li>
-  <li><a href="">Задание 3-5</a></li>
-  <li><a href="">Задание 3-6</a></li>
+  <p><a href="laba-3/s3-1.html" class="current">Задача 3.1</a></p>
+    <p><a href="laba-3/s3-2.php" class="current">Задача 3.2</a></p>
+    <p><a href="laba-3/s3-3.php" class="current">Задача 3.3</a></p>
+    <p><a href="laba-3/s3-4.php" class="current">Задача 3.4</a></p>
+    <p><a href="laba-3/s3-5.php" class="current">Задача 3.5</a></p>
+    <p><a href="laba-3/s3-6.html" class="current">Задача 3.6</a></p>
 </ul>
 </td>
-	</body>
+        
+</ul>
+    </body>
 </html>
