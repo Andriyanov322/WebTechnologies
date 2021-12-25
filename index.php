9cbf50631dea616504cc1451c1b5a2f9701281c3
Andriyanov322 <BombatGame@gmail.com>
2021-12-25 16:09:14 +0500

    Добавил первое задание второй лабораторной работы, а также кнопки для будущих задач

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html>
 <title> Главная страница Андриянова Максима</title>
+<table border="0" cellspacing="20">
 	<head>
 		<meta charset="utf-8">
 	</head>
 	<body>
+   <tr>
+    <th align="left">Лабораторная работа №1:</th>
+    <th align="left">Лабораторная работа №2:</th>
+   </tr>
+<tr>
+<td>
 <p> Дата и время:
 <p> 
 <?php
@@ -33,5 +40,19 @@
 	<li><a href="lab-1/lab-2-10.php" class="current">Задача 1-2</a></li>
 	<li><a href="lab-1/lab-2-11.php" class="current">Задача 1-3</a></li>
 </ul>
+</td>
+<td style="vertical-align:top">
+<ul>
+  <li><a href="lab-2/lab-2-1.php">Задание 2-1</a></li>
+  <li><a href="lab-2/lab-2-2.php">Задание 2-2</a></li>
+  <li><a href="lab-2/lab-2-3.php">Задание 2-3</a></li>
+</ul>
+<p> Самостоятельная работа </p>
+<ul>
+  <li><a href="lab-2/lab-2-4.php">Задача 2-4</a></li>
+  <li><a href="lab-2/lab-2-5.php">Задача 2-5</a></li>
+  <li><a href="lab-2/lab-2-6.php">Задача 2-6</a></li>
+</ul>
+</td>
 	</body>
 </html>
